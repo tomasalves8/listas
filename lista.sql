bc13e81722039d7d235c5b251ef72992a93b7cf1
Tomás Alves <tomasalves35@gmail.com>
2019-01-26 11:43:21 +0000

    Update SQL V2

--- lista.sql
+++ lista.sql
@@ -11,7 +11,28 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Dumping database structure for lista
+CREATE DATABASE IF NOT EXISTS `lista` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `lista`;
+
+-- Dumping structure for table lista.alunos
+CREATE TABLE IF NOT EXISTS `alunos` (
+  `numeros` int(255) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `votou` int(1) DEFAULT '0',
+  PRIMARY KEY (`numeros`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
+-- Dumping structure for table lista.votos
+CREATE TABLE IF NOT EXISTS `votos` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `Lista` char(1) NOT NULL,
+  `Votos` int(255) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
