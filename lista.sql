e5e685fed2ddc18f33de25c03e464d7396ade69e
Tomás Alves <tomasalves35@gmail.com>
2019-01-26 11:42:36 +0000

    SQL File Update

--- lista.sql
+++ lista.sql
@@ -1,84 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 4.8.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 28-Nov-2018 às 16:44
--- Versão do servidor: 10.1.33-MariaDB
--- PHP Version: 7.2.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Anfitrião:                    127.0.0.1
+-- Versão do servidor:           10.1.37-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Versão:              9.5.0.5196
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
---
--- Database: `lista`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `alunos`
---
-
-CREATE TABLE `alunos` (
-  `numeros` int(11) NOT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `votou` int(1) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `alunos`
---
-
-INSERT INTO `alunos` (`numeros`, `password`, `votou`) VALUES
-(17249, '$1$123$12bctNxJ9VoUltoF6xIeA0', 0),
-(25565, '$1$123$nE5gIYTYiF1PIXVOFjQaW/', 1);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `votos`
---
-
-CREATE TABLE `votos` (
-  `Lista` char(1) NOT NULL,
-  `Votos` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `votos`
---
-
-INSERT INTO `votos` (`Lista`, `Votos`) VALUES
-('g', 42),
-('m', 19),
-('v', 42);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `alunos`
---
-ALTER TABLE `alunos`
-  ADD PRIMARY KEY (`numeros`);
-
---
--- Indexes for table `votos`
---
-ALTER TABLE `votos`
-  ADD PRIMARY KEY (`Lista`);
-COMMIT;
-
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
